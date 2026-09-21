5c9c82b0e57e490d014e7fd354401dd5e9968154
vivek-jangela <vivekjangela@gmail.com>
2026-09-22 00:29:37 +0530

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/burn-in-frontend/"
+  base: "/burn-in-frontend/",
 })
