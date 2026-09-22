daa347dcb9012d60eb6b61b99fd89c64e118ab3e
vivek-jangela <vivekjangela@gmail.com>
2026-09-22 16:40:54 +0530

    Update CORS middleware allowed origins

--- backend/main.py
+++ backend/main.py
@@ -32,8 +32,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
+         "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://vivek-jangela.github.io",
+        # "http://localhost:5173",
+        # "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
